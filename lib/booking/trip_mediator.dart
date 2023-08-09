66322ce0839135ca7fed3f65698674b3fc369034
saed <samersame404@gmail.com>
2023-08-09 14:10:51 +0300

    add TripMediator

---
+++ lib/booking/trip_mediator.dart
@@ -0,0 +1,47 @@
+
+import 'package:latlong2/latlong.dart';
+
+class TripMediator {
+  LatLng startLocation;
+  LatLng endLocation;
+  double distance;
+  double duration;
+  String startLocationName;
+  String endLocationName;
+  int carTypeId;
+
+  TripMediator({
+    required this.startLocation,
+    required this.endLocation,
+    required this.distance,
+    required this.duration,
+    required this.startLocationName,
+    required this.endLocationName,
+    required this.carTypeId,
+  });
+
+  bool get startIsSet => startLocation.hashCode != 0;
+
+  bool get endIsSet => endLocation.hashCode != 0 ;
+
+  bool get startEnable => startLocation.hashCode == 0;
+
+  bool get endEnable => endLocation.hashCode == 0 && !startEnable;
+
+  bool get endInitial => endLocation.hashCode == 0;
+
+  bool get canConfirm => startLocation.hashCode > 0 && endLocation.hashCode > 0;
+
+
+  factory TripMediator.initial() {
+    return TripMediator(
+      startLocation: const LatLng(0, 0),
+      endLocation: const LatLng(0, 0),
+      distance: 0,
+      duration: 0,
+      carTypeId: 0,
+      startLocationName: '',
+      endLocationName: '',
+    );
+  }
+}
